d4f4f826f75cff80646309d7cba9f8dce95d5d97
quantum-corn <akashkhetan044@gmail.com>
2022-02-28 00:21:06 +0530

    v2.0

--- rectifier_half.py
+++ rectifier_half.py
@@ -10,11 +10,20 @@ schemdraw.use('svg')
 elm=schemdraw.elements
 d=schemdraw.Drawing(file='rectifier_half_ckt.svg')
 d+=elm.SourceSin().reverse().up()
-d+=elm.Line().right()
-d+=elm.Diode().down()
-d+=elm.Line().left()
+d+=(dio:=elm.Diode()).right()
+d+=elm.Line(unit=1)
+d+=elm.Resistor().down().label('$R_L$', loc='bottom')
+d+=elm.Line(unit=1).left()
+d+=elm.Line()
 d+=elm.Ground()
+
 d.draw()
+
+# %% with capacitor filter
+f=schemdraw.Drawing(file='rectifier_half_flt_ckt.svg')
+f+=elm.ElementDrawing(d)
+f+=elm.Capacitor().down().at(dio.end)
+f.draw()
 
 # %% md
 # ## Now the simulation
@@ -32,12 +41,19 @@ logger = Logging.setup_logging()
 circuit = Circuit("Half Wave Rectifier")
 circuit.include('../model_library/diode.lib')
 source=circuit.SinusoidalVoltageSource('input', 'input', circuit.gnd, amplitude=5@u_V, frequency=50@u_Hz)
-circuit.D('', 'input', 'R', model='1N4148')
-circuit.R('', 'R', circuit.gnd, 100@u_Ohm)
+circuit.D('', 'input', 'load', model='1N4148')
+circuit.R('', 'load', circuit.gnd, 100@u_Ohm)
 
 # %% simulate
 simulator = circuit.simulator()
 analysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
+
+# %% filter
+circuit.C('f', 'load', circuit.gnd, 1.25@u_mF)
+
+# %% simulate filter
+simulator = circuit.simulator()
+fanalysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
 
 # %% md
 # ### Now let's plot the data
@@ -53,7 +69,8 @@ ax.grid()
 ax.axhline(y=0, color='black')
 ax.axvline(x=0, color='black')
 ax.plot(analysis.input, label='input')
-ax.plot(analysis.R, label='output')
+ax.plot(analysis.load, label='unfiltered output')
+ax.plot(fanalysis.load, label='filtered output')
 plt.legend()
 plt.show()
 plt.savefig('rectifier_half_plt.svg')
